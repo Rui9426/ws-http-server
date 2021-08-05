dd76e0e8f08fdfd72bc8435451c14cb4f8659fa8
Yuri Zhao <36812463+Rui9426@users.noreply.github.com>
2021-08-05 14:45:02 +0800

    ->Sekeleton

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,20 +1,56 @@
-mod http_properties;
-use std::net::TcpListener;
+use std::error::Error;
+pub mod protocol;
+mod obj {
+    use std::{alloc::System, net::{TcpListener, TcpStream}};
 
-struct HttpServer {
-    tcp_listener: TcpListener,
-    handler: Handler<HttpRequest>,
+    use crate::logging::debug;
 
-}
-trait Handler<T> {
+    pub struct HttpRequest {}
 
-    fn handle(&self, t: T);
-}
-impl Handler<HttpRequest> for HttpServer {
-    fn handle(&self, t: HttpRequest) {
-        
+    pub trait InBoundHandler<T> {
+        fn handle(&self, t: T);
+    }
+
+    struct ThreadPool {}
+
+    impl ThreadPool {
+        fn execute(&self) {}
+
+        fn new(size: usize) -> ThreadPool {
+            ThreadPool {
+
+            }
+        }
+    }
+
+
+    pub struct HttpServer {
+        listener: TcpListener,
+        thread_pool: ThreadPool,
+    }
+
+    impl HttpServer {
+        pub fn accept<T: FnOnce(TcpStream) + Copy>(&self, handler: T) {
+            debug("Running TcpLitener...", crate::logging::LEVEL::DEBUG);
+            for stream in self.listener.incoming() {
+                let stream = stream.unwrap();
+                // handler.handle(stream);
+                handler(stream);
+                debug("Connection established", crate::logging::LEVEL::DEBUG);
+            }
+        }
+
+        pub fn new(addr: &str) -> HttpServer {
+            let tcp_listener = TcpListener::bind(addr).unwrap();
+            HttpServer {
+                listener: tcp_listener,
+                thread_pool: ThreadPool::new(8),
+            }
+        }
     }
 }
-struct HttpRequest {
 
+use crate::http::obj::HttpServer;
+pub fn create_http_server(addr: &str) -> Result<HttpServer, Box<dyn Error>> {
+    return Ok(HttpServer::new(addr));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,36 @@
+use std::{
+    fs,
+    io::{Read, Write},
+    net::TcpStream,
+};
+
+use logging::debug;
+
 mod http;
+mod logging;
+
+use crate::http::protocol::resp;
 fn main() {
-    
+    let http = http::create_http_server("localhost:8080").unwrap();
+
+    http.accept(|mut stream| {
+        let mut buffer = [0u8; 1024];
+        stream.read(&mut buffer).unwrap();
+        let resp_line = resp::get_ok_resp_status_line();
+        let content = fs::read_to_string("index.html").unwrap();
+
+        stream
+            .write(
+                format!(
+                    "{0}Content-Length: {1}\r\n\r\n{2}",
+                    resp_line,
+                    content.len(),
+                    content
+                )
+                .as_bytes(),
+            )
+            .unwrap();
+        stream.flush().unwrap();
+    });
+    debug("Hello", logging::LEVEL::INFO)
 }
